a88c1c6d693f7b7d033af125c55719b639bb7207
LVladDev <lvaldyslav6@gmail.com>
2023-07-14 22:47:28 -0700

    pro

--- src/views/Auth/components/ForgotPasswordForm.js
+++ src/views/Auth/components/ForgotPasswordForm.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Input from "../../../components/UI/Input/Input";
 import { Link } from "react-router-dom";
 
-
 const ForgotPasswordForm = () => {
 	const [form, setForm] = useState({});
 
